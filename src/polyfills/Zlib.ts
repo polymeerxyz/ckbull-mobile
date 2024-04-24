94732884d9b9bdeebe3458eafbccb138a4609faa
pablo <silvap.javier@gmail.com>
2024-04-24 19:13:13 +0200

    fix lint

--- src/polyfills/Zlib.ts
+++ src/polyfills/Zlib.ts
@@ -4,16 +4,10 @@
 
 // Función para comprimir datos utilizando el algoritmo de compresión DEFLATE.
 export function deflate(data: Uint8Array): Uint8Array {
-    // Tu implementación de DEFLATE aquí
-    // Esta implementación es solo un marcador de posición y no es funcional.
-    console.warn("Implementación de DEFLATE no proporcionada.");
     return data;
 }
 
 // Función para descomprimir datos utilizando el algoritmo de descompresión DEFLATE.
 export function inflate(data: Uint8Array): Uint8Array {
-    // Tu implementación de la descompresión DEFLATE aquí
-    // Esta implementación es solo un marcador de posición y no es funcional.
-    console.warn("Implementación de inflado no proporcionada.");
     return data;
 }
